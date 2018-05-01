c8b12d053b8f96f4889cc7d6f15a828b5151234b
ronakchittora <38683773+ronakchittora@users.noreply.github.com>
2018-05-02 04:47:12 +0530

    Update and rename ex6.py to String_lists.py

--- String_lists.py
+++ String_lists.py
@@ -1,3 +1,7 @@
+"""
+Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
+"""
+
 wrd=input("Please enter a word")
 wrd=str(wrd)
 rvs=wrd[::-1]
