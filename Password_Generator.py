44be571a4d10371f3904ed00a99f546ef74df1c3
ronakchittora <38683773+ronakchittora@users.noreply.github.com>
2018-05-03 01:10:11 +0530

    Update and rename ex16.py to Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,3 +1,9 @@
+"""
+Write a password generator in Python. Be creative with how you generate passwords - strong passwords have a mix of lowercase letters, uppercase letters, numbers, and symbols. The passwords should be random, generating a new password every time the user asks for a new password. Include your run-time code in a main method.
+
+Extra:
+Ask the user how strong they want their password to be. For weak passwords, pick a word or two from a list.
+"""
 from random import sample, choice, randint, shuffle
 
 def gen_password(type):
